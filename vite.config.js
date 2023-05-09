53984b0738f128bda5f50452dcdeff533c65ce9d
Alexsandr <alssval@gmail.com>
2023-05-09 05:20:54 +0300

    finall

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-movies/",
+  base: "/react-food/",
   plugins: [preact()],
 })
